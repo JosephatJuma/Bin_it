9b2d78af95998efb830c0d7c6597f5ddbc547f21
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-08-21 12:37:25 +0300

    Make minor changes in app.js

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const app = express();
-const path = require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const config = require("./config");
-const session = require("express-session");
+
 const cors = require("cors");
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 //configure express session
 app.use(express.urlencoded({ extended: true }));
-app.use(session({ secret: "Token", resave: false, saveUninitialized: true }));
-app.use(express.static(path.join(__dirname, "public")));
+
 //connect to database
 mongoose.connect(config.database, {
   useNewUrlParser: true,
